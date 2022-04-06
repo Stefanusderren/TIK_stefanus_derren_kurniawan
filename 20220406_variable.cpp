64e093990759630f69c84798a19e1495c1def534
Stefanusderren <103085641+Stefanusderren@users.noreply.github.com>
2022-04-05 20:09:03 -0700

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main() {
+	float a = 3.14, b = 10.21;
+	cout<<a+b;
+}
