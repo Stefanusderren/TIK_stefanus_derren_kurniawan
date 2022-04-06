f6ad72ab4fc099035fee9455be8fb7b433955c99
Stefanusderren <103085641+Stefanusderren@users.noreply.github.com>
2022-04-05 20:27:14 -0700

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main() {
+	string nama;
+	cin>>nama;
+	cout<<"nama anda adalah: "<<nama<<endl;
+
+	return 0;	
+}
